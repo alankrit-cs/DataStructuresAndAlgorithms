31293cccbed27ceb0c3b5beca802cc29fe8b3387
alankrit-cs <108877396+alankrit-cs@users.noreply.github.com>
2024-09-05 23:20:10 +0530

    Created sliding window controller with 2 examples

---
+++ Controllers/ArraysAndStrings/SlidingWindowController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace DataStructuresAndAlgorithms.Controllers.ArraysAndStrings
+{
+    [ApiController]
+    [Route("api/ArraysAndStrings/[controller]")]
+    public class SlidingWindowController : Controller
+    {
+        [HttpPost("LongestSubarraySumIsLessThanK")]
+        [SwaggerOperation(
+            Summary = "Given an array of positive integers nums and an integer k, find the length of the longest subarray whose sum is less than or equal to k")]
+        public IActionResult FindLength([FromBody] List<int> nums, int target)
+        {
+            int left = 0, ans = 0, curr = 0;
+
+            for(int right = 0; right < nums.Count; right++)
+            {
+                curr = curr + nums[right];
+
+                while (curr > target)
+                {
+                    curr = curr - nums[left];
+                    left++;
+                }
+
+                ans = Math.Max(ans, right - left + 1);
+            }
+
+            return Ok(ans);
+        }
+
+        [HttpPost("LongestSubstringOfOnes")]
+        [SwaggerOperation(Summary = "You are given a binary string s (a string containing only \"0\" and \"1\"). You may choose up to one \"0\" and flip it to a \"1\". What is the length of the longest substring achievable that contains only \"1\"",
+            Description = "For example, given s = \"1101100111\", the answer is 5. If you perform the flip at index 2, the string becomes 1111100111")]
+        public IActionResult LongestSubstringOfOnes(string str)
+        {
+            int left = 0, curr = 0, ans = 0;
+
+            for(int right = 0; right<str.Length; right++)
+            {
+                if (str[right] == '0')
+                {
+                    curr = curr + 1;
+                }
+
+                while(curr > 1)
+                {
+                    if (str[left] == '0')
+                    {
+                        curr = curr - 1;
+                    }
+                    left++;
+                }
+
+                ans = Math.Max(ans, right - left + 1);
+            }
+            return Ok(ans);
+        }
+    }
+}
--- Controllers/ArraysAndStrings/TwoPointersController.cs
+++ Controllers/ArraysAndStrings/TwoPointersController.cs
@@ -35,12 +35,13 @@ namespace DataStructuresAndAlgorithms.Controllers.ArraysAndStrings
             Summary = "Given a sorted array of unique integers and a target integer, return true if there exists a pair of numbers that sum to target, false otherwise.",
             Description = "This problem is similar to Two Sum. (In Two Sum, the input is not sorted). For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, return true because 4 + 9 = 13."
         )]
-        public IActionResult CheckForTarget([FromBody]List<int> nums, int target)
+        public IActionResult CheckForTarget([FromBody] List<int> nums, int target)
         {
             int left = 0;
             int right = nums.Count - 1;
 
-            while(left < right){
+            while (left < right)
+            {
                 int value = nums[left] + nums[right];
 
                 if (value == target)
@@ -57,6 +58,71 @@ namespace DataStructuresAndAlgorithms.Controllers.ArraysAndStrings
                     left++;
                 }
             }
+            return Ok(false);
+        }
+
+        [HttpPost("SortCombined")]
+        [SwaggerOperation(
+            Summary = "Given two sorted integer arrays arr1 and arr2, return a new array that combines both of them and is also sorted"
+        )]
+        public IActionResult SortCombined([FromQuery] List<int> arr1, [FromQuery] List<int> arr2)
+        {
+            int i = 0, j = 0;
+            var sortedArray = new List<int>();
+
+            while (i < arr1.Count && j < arr2.Count)
+            {
+                if (arr1[i] < arr2[j])
+                {
+                    sortedArray.Add(arr1[i]);
+                    i++;
+                }
+                else
+                {
+                    sortedArray.Add(arr2[j]);
+                    j++;
+                }
+            }
+
+            while (i < arr1.Count)
+            {
+                sortedArray.Add(arr1[i]);
+                i++;
+            }
+
+            while (j < arr2.Count)
+            {
+                sortedArray.Add(arr2[j]);
+                j++;
+            }
+
+            return Ok(sortedArray);
+        }
+
+        [HttpPost("IsSubsequence.")]
+        [SwaggerOperation(
+            Summary = "Given two strings s and t, return true if s is a subsequence of t, or false otherwise.",
+            Description = "A subsequence of a string is a sequence of characters that can be obtained by deleting some (or none) of the characters from the original string, while maintaining the relative order of the remaining characters. For example, \"ace\" is a subsequence of \"abcde\" while \"aec\" is not."
+        )]
+        public IActionResult IsSubsequence([FromQuery] string s,[FromQuery] string t)
+        {
+            int i = 0, j = 0;
+
+            while(i < s.Length && j < t.Length)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                }
+
+                j++;
+            }
+
+            if(i == s.Length)
+            {
+                return Ok(true);
+            }
+
             return Ok(false);
         }
     }
